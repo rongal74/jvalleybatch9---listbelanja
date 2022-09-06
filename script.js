03b9d67ac9b705eb8b9f738e6c499b41f7898efa
rongal74 <aaron.galag@gmail.com>
2022-09-06 07:22:11 +0700

    3 files modified

--- script.js
+++ script.js
@@ -5,33 +5,34 @@ let floating_button = document.getElementById('floating_button');
 let addlist_form = document.getElementById('addlist_form');
 let root = document.getElementById('root');
 let subtitle = document.getElementById('subtitle');
-
-if (subtitle) {
-  subtitle.innerHTML = new Date().toLocaleDateString();
-}
+let total = 0;
 
 // Data List Belanja
 let data_list_belanja = [];
 
+subtitle.innerHTML = new Date().toLocaleDateString();
+
+root.innerHTML = `
+<div class="total">
+  <p>Total : Rp. ${numberWithCommas(total)},-</p>
+</div>
+`;
+
 // tambahkan event listener
-if (floating_button) {
-  floating_button.addEventListener('click', () => {
-    // Set style pada modal display -> flex
-    if (modal.style.display == 'none') {
-      showModal();
-      return;
-    }
-    // sembunyikan modal
-    hideModal();
-  });
-}
+floating_button.addEventListener('click', () => {
+  // Set style pada modal display -> flex
+  if (modal.style.display == 'none') {
+    showModal();
+    return;
+  }
+  // sembunyikan modal
+  hideModal();
+});
 
 // Menambahkan event listener pada modal_bg
-if (modal_bg) {
-  modal_bg.addEventListener('click', () => {
-    hideModal();
-  });
-}
+modal_bg.addEventListener('click', () => {
+  hideModal();
+});
 
 function showModal() {
   modal.style.display = 'flex';
@@ -45,37 +46,38 @@ function hideModal() {
   floating_button.style.transform = 'rotate(0deg)';
 }
 
-if (addlist_form) {
-  addlist_form.addEventListener('submit', (event) => {
-    // stop form dari reload page
-    event.preventDefault();
+addlist_form.addEventListener('submit', (event) => {
+  // stop form dari reload page
+  event.preventDefault();
 
-    // tangkap value dari masing-masing input field
-    let barang = event.target.barang.value;
-    let harga = event.target.harga.value;
+  // tangkap value dari masing-masing input field
+  let barang = event.target.barang.value;
+  let harga = event.target.harga.value;
 
-    // push data ke data_list_belanja
-    data_list_belanja.push({
-      nama_barang: barang,
-      harga_barang: harga,
-      tanggal: new Date().toLocaleDateString(),
-      waktu: new Date().toLocaleTimeString(),
-    });
-
-    // console.info(data_list_belanja);
-
-    // clear value input
-    event.target.barang.value = '';
-    event.target.harga.value = '';
-
-    hideModal();
-    renderToHtml();
+  // push data ke data_list_belanja
+  data_list_belanja.push({
+    nama_barang: barang,
+    harga_barang: harga,
+    tanggal: new Date().toLocaleDateString(),
+    waktu: new Date().toLocaleTimeString(),
   });
-}
+
+  // clear value input
+  event.target.barang.value = '';
+  event.target.harga.value = '';
+
+  hideModal();
+  renderToHtml();
+});
 
 function renderToHtml() {
   // clear elemen root
-  root.innerHTML = '';
+  hitungTotal();
+  root.innerHTML = `
+    <div class="total">
+      <p>Total : Rp. ${numberWithCommas(total)},-</p>
+    </div>
+  `;
 
   // Looping
   data_list_belanja.forEach((e, i) => {
@@ -83,9 +85,9 @@ function renderToHtml() {
       <div class="card">
         <small> ${e.tanggal} ${e.waktu} </small>
         <div> 
-          ${e.nama_barang} <span>Rp. ${numberWithCommas(
+          ${e.nama_barang} <span> Rp. ${numberWithCommas(
       e.harga_barang
-    )},-</span>
+    )},- </span>
         </div>
         <button onclick="handleDelete(${i})">Selesai</button>
       </div>
@@ -97,11 +99,17 @@ function renderToHtml() {
 
 const handleDelete = (index) => {
   data_list_belanja.splice(index, 1);
-
   renderToHtml();
 };
 
 // format number separator regex
 function numberWithCommas(num) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
+
+function hitungTotal() {
+  total = 0;
+  for (let i = 0; i < data_list_belanja.length; i++) {
+    total += parseInt(data_list_belanja[i].harga_barang);
+  }
 }
